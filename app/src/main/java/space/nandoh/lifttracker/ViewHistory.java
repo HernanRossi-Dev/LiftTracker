0ccdf94efd7b3db10e797ee84af81a34ed123df5
HernanRossi <hernan_rossi@msn.com>
2017-06-20 18:14:54 -0700

    Update comments

--- app/src/main/java/space/nandoh/lifttracker/ViewHistory.java
+++ app/src/main/java/space/nandoh/lifttracker/ViewHistory.java
@@ -1,11 +1,13 @@
 package space.nandoh.lifttracker;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,16 +21,18 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+import space.nandoh.lifttracker.adapters.RecyclerItemClickListener;
+
 /***************************************************************************************************
- **
- *  Created by Hernan Rossi.
- **                 Activity to search through the entire database of saved lifts and list them
- *                  in chronological order on list entry for every day with a title of the most
- *                  worked muscle group of that day whether it was a morning afternoon or evening
- *                  workout the users body weight if it was given for that day and the total weight
- *                  lifted over all of the lifts done that day. Clicking on a day in the list will
- *                  launch a new activity which will show the user a more detailed breakdown of
- *                  the lifts that were performed on that day an another other statistics available
+ * *
+ * Created by Hernan Rossi.
+ * *                 Activity to search through the entire database of saved lifts and list them
+ * in chronological order on list entry for every day with a title of the most
+ * worked muscle group of that day whether it was a morning afternoon or evening
+ * workout the users body weight if it was given for that day and the total weight
+ * lifted over all of the lifts done that day. Clicking on a day in the list will
+ * launch a new activity which will show the user a more detailed breakdown of
+ * the lifts that were performed on that day an another other statistics available
  **************************************************************************************************/
 public class ViewHistory extends Activity {
 
@@ -36,7 +40,7 @@ public class ViewHistory extends Activity {
     private ArrayList<String> day_array = new ArrayList<>();
     private String selected_day; // The day that has been selected by the user to see complete breakdown of
 
-    public ViewHistory(){
+    public ViewHistory() {
     }
 
 
@@ -44,15 +48,15 @@ public class ViewHistory extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_history);
-        if(savedInstanceState!=null){
+        if (savedInstanceState != null) {
         }
-        try{
+        try {
             SQLiteOpenHelper liftDatabaseHelper = new LiftDatabaseHelper(this);
             SQLiteDatabase db = liftDatabaseHelper.getReadableDatabase();
             // Get all of the individual days that are in the history table
             // .query(tableName, tableColumns, whereClause, whereArgs, groupBy, having, orderBy);
             String tableName = "SAVE_DATA";
-            String[] tableColumns = new String[] {"DISTINCT DATE"};
+            String[] tableColumns = new String[]{"DISTINCT DATE"};
             String whereClause = null;
             String[] whereArgs = null;
             String groupBy = null;
@@ -65,11 +69,11 @@ public class ViewHistory extends Activity {
                     groupBy,
                     having,
                     orderBy);
-            if(cursor_days.moveToFirst()){
+            if (cursor_days.moveToFirst()) {
                 String temp_day = cursor_days.getString(0);
                 String result_day = temp_day.replace("\\", " ");
                 day_array.add(result_day);
-                while(cursor_days.moveToNext()){
+                while (cursor_days.moveToNext()) {
                     temp_day = cursor_days.getString(0);
                     result_day = temp_day.replace("\\", " ");
                     day_array.add(result_day);
@@ -77,7 +81,7 @@ public class ViewHistory extends Activity {
             }
             db.close();
             cursor_days.close();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
@@ -94,19 +98,76 @@ public class ViewHistory extends Activity {
         super.onStart();
         final String[] days = new String[day_array.size()];
         day_array.toArray(days);
-        ListView listView = (ListView)findViewById(R.id.listview_history);
-        TextView textView = (TextView)findViewById(R.id.history_title);
-        String titleText = (String)textView.getText();
+        ListView listView = (ListView) findViewById(R.id.listview_history);
+        TextView textView = (TextView) findViewById(R.id.history_title);
+        String titleText = (String) textView.getText();
         HashMap<String, String> date = MainActivity.getDate();
         titleText = titleText + "\nToday is " + date.get("day_word") + " the " + date.get("day_number");
         textView.setText(titleText);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            public void onItemClick(AdapterView parentView, View childVIew,
-                                    int position, long id ) {
-                // Method to run when an item in the list of days is clicks
-                selected_day = days[position].toString();
-            }
-        });
-        listView.setAdapter(adapter);
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//            public void onItemClick(AdapterView parentView, View childVIew,
+//                                    int position, long id ) {
+//                // Method to run when an item in the list of days is clicks
+//                selected_day = days[position].toString();
+//            }
+//        });
+//        listView.setAdapter(adapter);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.listview_history);
+        final Context context = this.getApplicationContext();
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(context, recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        selected_day = days[position].toString();
+                        try {
+                            SQLiteOpenHelper liftDatabaseHelper = new LiftDatabaseHelper(context);
+                            SQLiteDatabase db = liftDatabaseHelper.getReadableDatabase();
+                            String tableName = "SAVE_DATA";
+                            String[] tableColumns = new String[]{"*"};
+                            String query_day = selected_day.replace(" ", "\\");
+                            String whereClause = "DATE=" + query_day;
+                            String[] whereArgs = null;
+                            String groupBy = null;
+                            String having = null;
+                            String orderBy = null;
+                            Cursor cursor_days = db.query(tableName,
+                                    tableColumns,
+                                    whereClause,
+                                    whereArgs,
+                                    groupBy,
+                                    having,
+                                    orderBy);
+                            if (cursor_days.moveToFirst()) {
+                                String lift_name = cursor_days.getString(1);
+                                String muscle_group = cursor_days.getString(2);
+                                String weight_type = cursor_days.getString(3);
+                                String reps_weight = cursor_days.getString(4);
+                                /*TODO
+                                output lift to the view
+                                 */
+                                while (cursor_days.moveToNext()) {
+                                    lift_name = cursor_days.getString(1);
+                                    muscle_group = cursor_days.getString(2);
+                                    weight_type = cursor_days.getString(3);
+                                    reps_weight = cursor_days.getString(4);
+                                    /*TODO
+                                output lift to the view
+                                 */
+                                }
+                            }
+                            db.close();
+                            cursor_days.close();
+                        } catch (SQLException e) {
+                            Toast toast = Toast.makeText(context, "Database unavailable", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+                        // do whatever
+                    }
+                })
+        );
     }
 }
